93f7b941979f784ce8d45cda3f17ec48f473762d
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:53:59 +0000

    Update Calculadora.java

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Calculadora {
+
+public class Calculadora {
+
+    public int calcularMCD(int num1, int num2) {
+        while(num1 != num2) {
+            if(num1 > num2) {
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+        }
+        return num1;
+    }
 }
