04f09c688f1e7efe4369572034d63c20b33b303f
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:58:32 +0000

    Update ToDoList.java

--- src/main/java/org/example/ToDoList.java
+++ src/main/java/org/example/ToDoList.java
@@ -1,2 +1,17 @@
-package org.example;public class ToDoList {
+package org.example;
+package com.ejemplo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<String> tasks = new ArrayList<>();
+
+    public void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
 }
