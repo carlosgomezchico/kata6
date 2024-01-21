11485dee9fb14efdff7d80daf8cb0156fc8d8053
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:59:09 +0000

    Update ToDoListTest.java

--- src/test/java/org/example/ToDoListTest.java
+++ src/test/java/org/example/ToDoListTest.java
@@ -1,2 +1,18 @@
-package org.example;public class ToDoListTest {
+
+package com.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+
+public class ToDoListTest {
+
+    @Test
+    public void testAddTask() {
+        ToDoList toDoList = new ToDoList();
+        toDoList.addTask("Aprender TDD");
+        List<String> expected = Arrays.asList("Aprender TDD");
+        assertEquals(expected, toDoList.getTasks());
+    }
 }
